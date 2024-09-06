48a2c8a34ed3a0f6d5f974a1878861d164f74a68
mishv <mishv@LAPTOP-T7BS8B0K>
2024-09-06 09:24:30 +0300

    Добавьте файлы проекта.

---
+++ lab_1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab_1.Models;
+using System;
+
+namespace lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(0, 101);
+            var company = new Company
+            {
+                Name = "GuukPeef",
+                Address = "123 Vashington Street, NewYork",
+                RandomNumber = number
+            };
+
+            return Json(company);
+        }
+
+    }
+}
---
+++ lab_1/Models/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab_1.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int RandomNumber { get; set; }
+    
+    }
+}
